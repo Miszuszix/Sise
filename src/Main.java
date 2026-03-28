0fb1045c828cc34f629b751302d9dc061def5260
Miszuszix <adels123456@gmail.com>
2026-03-28 15:35:57 +0100

    test harder problem

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 void main() {
     State goalState = new State(new int[][]{
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 0}},
-            2, 2);
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 0}}, 
+            3, 3);
     State initialState = new State(new int[][]{
-            {1, 2, 3},
-            {4, 5, 6},
-            {0, 7, 8}},
-            0, 2);
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {10, 0, 11, 12},
+            {9, 13, 14, 15}},
+            1, 2);
     Graph testGraph = new Graph(goalState);
     System.out.println(BFS(testGraph, new Node(initialState)));
 }
